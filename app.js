bb06705c3716ce2d10c615d537ba37e006a06ab6
YuriSantuche <yurisantuche@gmail.com>
2025-01-23 12:33:27 -0300

    projeto funcional

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let nomesAdicionados = document.querySelector('p');
+let caixaComNomes = document.getElementById('amigo');
+
+
+function adicionarAmigo(){
+    if(caixaComNomes.value == ""){
+        alert("Por favor, insira um nome.")
+    }
+    else{
+        amigos.push(caixaComNomes.value);
+        nomesAdicionados.innerHTML += (`${caixaComNomes.value} `);
+        caixaComNomes.value = '';
+    }
+}
+
+
+function sortearAmigo(){
+    if(amigos.length == 0){
+        alert('Nenhum nome foi adicionado.');
+    }
+    else{
+        let indiceAleatorio = Math.floor(Math.random()*amigos.length);
+        let nomeSorteado = amigos[indiceAleatorio];
+        console.log(nomeSorteado);
+        nomesAdicionados.innerText = `O Sorteado foi ${nomeSorteado}`;
+
+    }
+}
+
 
